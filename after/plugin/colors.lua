547a4c55dc580a1b069301eba7c1ba140cc6ea11
Christian Hageloch <christian.hageloch@gmail.com>
2022-09-19 17:08:21 +0200

    Some changes to the theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,18 @@
 vim.g.gruvbox_baby_function_style = "bold"
 vim.g.gruvbox_baby_keyword_style = "bold"
-vim.g.gruvbox_baby_highlights = {Normal = {fg = "#fbf1c7", bg = "NONE", style="NONE"}}
+vim.g.gruvbox_baby_telescope_theme = 1
+vim.g.gruvox_baby_background_color = "dark"
+vim.g.gruvbox_baby_highlights = {
+    Normal = {
+        fg = "#fbf1c7",
+        bg = "NONE",
+        style="NONE"
+    },
+    SignColumn = {
+        fg = "NONE",
+        bg = "NONE",
+        style = "NONE"
+    }
+}
 
 vim.cmd[[colorscheme gruvbox-baby]]
