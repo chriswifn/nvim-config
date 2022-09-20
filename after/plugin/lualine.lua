16cf8697ced8bf6180465bce859ffaa6ef6bb9e4
Christian Hageloch <christian.hageloch@gmail.com>
2022-09-20 20:48:01 +0200

    Remove lualine

--- after/plugin/lualine.lua
+++
@@ -1,7 +0,0 @@
-require('lualine').setup{
-    options = {
-        theme = 'gruvbox-baby',
-        component_separators = { left = ' ', right = ' '},
-        section_separators = { left = ' ', right = ' '},
-    }
-}
--- lua/chris/packer.lua
+++ lua/chris/packer.lua
@@ -21,7 +21,6 @@ require('packer').startup(function(use)
     use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use 'numToStr/Comment.nvim'
-    use 'nvim-lualine/lualine.nvim'
     use 'lewis6991/gitsigns.nvim'
     use 'norcalli/nvim-colorizer.lua'
     use {
