67c0a70af3b4900cf222341396907ace075a5b86
Christian Hageloch <christian.hageloch@gmail.com>
2022-09-20 09:38:46 +0200

    Comments should not be italic

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,6 @@
 vim.g.gruvbox_baby_function_style = "bold"
 vim.g.gruvbox_baby_keyword_style = "bold"
+vim.g.gruvbox_baby_comment_style = "NONE"
 vim.g.gruvbox_baby_telescope_theme = 1
 vim.g.gruvox_baby_background_color = "dark"
 vim.g.gruvbox_baby_highlights = {
