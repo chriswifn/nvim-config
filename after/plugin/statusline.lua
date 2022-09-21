48e1bde967eb2d05af252e6aa4b55851b30803a6
Christian Hageloch <christian.hageloch@gmail.com>
2022-09-21 08:43:48 +0200

    el

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -1,51 +1,5 @@
-local vcs = function()
-  local git_info = vim.b.gitsigns_status_dict
-  if not git_info or git_info.head == "" then
-    return ""
-  end
-  local added = git_info.added and ("+" .. git_info.added .. " ") or ""
-  local changed = git_info.changed and ("~" .. git_info.changed .. " ") or ""
-  local removed = git_info.removed and ("-" .. git_info.removed .. " ") or ""
-  if git_info.added == 0 then
-    added = ""
-  end
-  if git_info.changed == 0 then
-    changed = ""
-  end
-  if git_info.removed == 0 then
-    removed = ""
-  end
-  return table.concat {
-     added,
-     changed,
-     removed,
-     git_info.head,
-     " ",
-  }
-end
-
-Statusline = {}
-
-Statusline.active = function()
-    return table.concat {
-        "%f",
-        "%=",
-        vcs(),
-        "%l,%c",
-        " ",
-        "%p%%",
-    }
-end
-
-function Statusline.inactive()
-    return "%F"
-end
-
-vim.api.nvim_exec([[
-  augroup Statusline
-  au!
-  au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
-  au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
-  augroup END
-]], false)
-
+require('el').setup {
+  -- An example generator can be seen in `Setup`.
+  -- A default one is supplied if you do not want to customize it.
+  generator = default,
+}
