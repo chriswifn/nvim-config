af00a275d87f01b3473142432e47a8d3883468da
Christian Hageloch <christian.hageloch@gmail.com>
2022-10-28 17:33:33 +0200

    not "all"

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = "all",
+    ensure_installed = {"c", "lua", "bash", "javascript", "haskell", "php", "python", "rust", "go"},
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
