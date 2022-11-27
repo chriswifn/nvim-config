67809ce5d1effa36f860bd533c8313aa18c81cd5
Christian Hageloch <christian.hageloch@gmail.com>
2022-11-27 18:26:50 +0100

    git info in statusbar

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -32,6 +32,34 @@ local function mode()
   return string.format(" %s ", modes[current_mode]):upper()
 end
 
+local vcs = function()
+  local git_info = vim.b.gitsigns_status_dict
+  if not git_info or git_info.head == "" then
+    return ""
+  end
+  local added = git_info.added and ("+" .. git_info.added .. " ") or ""
+  local changed = git_info.changed and ("~" .. git_info.changed .. " ") or ""
+  local removed = git_info.removed and ("-" .. git_info.removed .. " ") or ""
+  if git_info.added == 0 then
+    added = ""
+  end
+  if git_info.changed == 0 then
+    changed = ""
+  end
+  if git_info.removed == 0 then
+    removed = ""
+  end
+  return table.concat {
+     " [",
+     git_info.head,
+     " ",
+     added,
+     changed,
+     removed,
+     "]"
+  }
+end
+
 local function lsp()
   local count = {}
   local levels = {
@@ -77,6 +105,7 @@ Statusline.active = function()
     return table.concat {
         mode(),
         " %F",
+        vcs(),
         "%=%#StatusLineExtra#",
         lsp(),
         lineinfo(),
