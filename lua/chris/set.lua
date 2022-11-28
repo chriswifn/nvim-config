2c7473bad641e737c58b060f42d30feea1f96529
Christian Hageloch <christian.hageloch@gmail.com>
2022-11-28 12:19:07 +0100

    autocmd to remove autocontinuation of comments

--- lua/chris/set.lua
+++ lua/chris/set.lua
@@ -29,7 +29,7 @@ vim.opt.updatetime = 50
 vim.opt.shortmess:append("c")
 vim.g.mapleader = " "
 -- disable continuation of comments
-vim.opt.formatoptions = vim.opt.formatoptions - {"c", "r", "o"}
+vim.api.nvim_create_autocmd("BufEnter", { callback = function() vim.opt.formatoptions = vim.opt.formatoptions - { "c","r","o" } end, })
 
 -- swapfile and backup disabled
 vim.opt.swapfile = false
