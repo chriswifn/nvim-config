81d9acc8931b9a521de8618b2da6e5939edde276
Christian Hageloch <christian.hageloch@gmail.com>
2022-12-26 15:48:56 +0100

    colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -12,7 +12,7 @@ require("tokyonight").setup({
         floats = "dark",
     },
     sidebars = { "qf", "help" },
-    day_brightness = 0.3,
+    -- day_brightness = 0.3,
     hide_inactive_statusline = false,
     dim_inactive = false,
     lualine_bold = false,
@@ -22,4 +22,9 @@ require("tokyonight").setup({
     on_highlights = function(highlights, colors) end,
 })
 
-vim.cmd[[colorscheme tokyonight]]
+vim.o.background = "dark"
+vim.cmd.colorscheme("tokyonight")
+
+function ColorMyPencils()
+    vim.cmd("source ~/.config/nvim/after/plugin/colors.lua")
+end
