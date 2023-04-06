d61e4b1b084bc145037ccb555691c9a441db317c
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-06 18:39:09 +0200

    add nvim-tree

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,24 @@
+local M = {
+    "nvim-tree/nvim-tree.lua",
+    event = "BufWinEnter",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    keys = {
+        { "<leader>tf", ":NvimTreeToggle<cr>", desc = "[T]oggle [F]ile-manager" }
+    },
+    opts = {
+        sort_by = "case_sensitive",
+        renderer = {
+            group_empty = true,
+        },
+        filters = {
+            dotfiles = true,
+        }
+    },
+    config = function(_, opts)
+        require("nvim-tree").setup(opts)
+    end
+}
+
+return M
