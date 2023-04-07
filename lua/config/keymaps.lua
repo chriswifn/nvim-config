a4b660b8f5e47a7b2b018311e152760ac19c8e1c
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-07 15:45:20 +0200

    few more keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,30 @@
 -- this is where keymaps are specified that are not linked to a plugin
 
 -- buffer navigation
-vim.keymap.set("n", "<leader>bp", ":bprev")
-vim.keymap.set("n", "<leader>bn", ":bnext")
+vim.keymap.set("n", "<leader>bp", ":bprev<cr>")
+vim.keymap.set("n", "<leader>bn", ":bnext<cr>")
 
 -- tmux_sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux_sessionizer<cr>")
+
+-- copy and paste (system clipboard)
+vim.keymap.set("n", "<leader>p", [["+p]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- move things around in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- center cursor when jumping with C-d and C-u
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- move cursor when going through search list
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- make file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
