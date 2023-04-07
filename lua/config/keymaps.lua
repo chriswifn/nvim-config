ef8b6a17fac73ca757c2328e929b51cbc399e95b
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-07 12:20:32 +0200

    adding some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,8 @@
 -- this is where keymaps are specified that are not linked to a plugin
+
+-- buffer navigation
+vim.keymap.set("n", "<leader>bp", ":bprev")
+vim.keymap.set("n", "<leader>bn", ":bnext")
+
+-- tmux_sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux_sessionizer<cr>")
