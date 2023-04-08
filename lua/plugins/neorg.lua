00062fc3a5568a1b722aba2b4771f7caff140a2e
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-08 10:53:57 +0200

    add modules to neorg to make it worth using

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -4,23 +4,52 @@ local M = {
     build = ":Neorg sync-parsers",
     opts = {
         load = {
-            ["core.defaults"] = {}, -- Loads default behaviour
-            ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+            ["core.defaults"] = {},
+            ["core.norg.concealer"] = {
+                config = {
+                    icon_preset = "diamond",
+                }
+            },
+            ["core.keybinds"] = {
+                config = {
+                    default_keybindings = true,
+                    hook = function(keybinds)
+                        keybinds.remap_key("traverse-heading", "n", "k", "<M-k>")
+                    end,
+                }
+            },
             ["core.norg.dirman"] = { -- Manages Neorg workspaces
                 config = {
                     workspaces = {
+                        main = "~/notes",
                         work = "~/notes/work",
-                        home = "~/notes/home",
                         wiki = "~/notes/wiki",
                         university = "~/notes/university",
                     },
+                    autochdir = false,
+                    default_workspace = "main",
                 },
+            },
+            ["core.integrations.treesitter"] = {},
+            ["core.presenter"] = {
+                config = {
+                    zen_mode = "truezen",
+                }
+            },
+            ["core.norg.journal"] = {},
+            ["core.export"] = {},
+            ["core.export.markdown"] = {
+                config = {
+                    extensions = "all",
+                }
             },
         },
     },
-    dependencies = { { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" } },
+    dependencies = { { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter", "nvim-neorg/neorg-telescope", "Pocco81/true-zen.nvim" } },
     config = function(_, opts)
         require("neorg").setup(opts)
+        require("telescope").load_extension("neorg")
+        require("true-zen").setup()
     end
 }
 
