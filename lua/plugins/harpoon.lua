85dd7b2a324b2a7786a24c41b4564e871ce9c10b
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-08 21:15:37 +0200

    descriptive description

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,11 +3,11 @@ local M = {
     event = "BufReadPre",
     keys = {
         { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "[H]arpoon [A]dd File" },
-        { "<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "[H]arpoon [A]dd File" },
-        { "<leader>hh", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "[H]arpoon [A]dd File" },
-        { "<leader>hj", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "[H]arpoon [A]dd File" },
-        { "<leader>hk", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "[H]arpoon [A]dd File" },
-        { "<leader>hl", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "[H]arpoon [A]dd File" },
+        { "<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "[H]arpoon [M]enu" },
+        { "<leader>hh", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "[H]arpoon [1]" },
+        { "<leader>hj", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "[H]arpoon [2]" },
+        { "<leader>hk", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "[H]arpoon [3]" },
+        { "<leader>hl", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "[H]arpoon [4]" },
     },
     config = function()
         require("harpoon").setup()
