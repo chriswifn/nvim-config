cddf5db7f71fd60153e696e5c980b72c3f1c85e6
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-23 20:51:41 +0200

    gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,58 +1,15 @@
 -- colorscheme
 local M = {
-    "folke/tokyonight.nvim",
-
+    "sainnhe/gruvbox-material",
     lazy = false,
-
     priority = 1000,
-
-    opts = function()
-        return {
-            style = "night",
-            hide_inactive_statusline = true,
-            transparent = true,
-            styles = {
-                keywords = { bold = true },
-                functions = { bold = true },
-            },
-            on_highlights = function(hl, c)
-                local prompt = "#2d3149"
-                hl.TelescopeNormal = {
-                    bg = c.bg_dark,
-                    fg = c.fg_dark,
-                }
-                hl.TelescopeBorder = {
-                    bg = c.bg_dark,
-                    fg = c.bg_dark,
-                }
-                hl.TelescopePromptNormal = {
-                    bg = prompt,
-                }
-                hl.TelescopePromptBorder = {
-                    bg = prompt,
-                    fg = prompt,
-                }
-                hl.TelescopePromptTitle = {
-                    bg = prompt,
-                    fg = prompt,
-                }
-                hl.TelescopePreviewTitle = {
-                    bg = c.bg_dark,
-                    fg = c.bg_dark,
-                }
-                hl.TelescopeResultsTitle = {
-                    bg = c.bg_dark,
-                    fg = c.bg_dark,
-                }
-            end,
-        }
-    end,
-
-    config = function(_, opts)
-        local tokyonight = require("tokyonight")
-        tokyonight.setup(opts)
-        tokyonight.load()
-    end,
+    config = function ()
+        vim.cmd("let g:gruvbox_material_foreground = 'original'")
+        vim.cmd("let g:gruvbox_material_background = 'hard'")
+        vim.cmd("let g:gruvbox_material_better_performance = 1")
+        vim.cmd("let g:gruvbox_material_enable_bold = 1")
+        vim.cmd("colorscheme gruvbox-material")
+    end
 }
 
 return M
