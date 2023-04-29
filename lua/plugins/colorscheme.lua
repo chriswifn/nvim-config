c1189731d01559139fb363af991afdce639de53e
Christian Hageloch <christian.hageloch@gmail.com>
2023-04-29 10:37:27 +0200

    remove telescope theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,40 +11,6 @@ local M = {
         keywords = { bold = true },
         functions = { bold = true },
       },
-      on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-          bg = prompt,
-        }
-        hl.TelescopePromptBorder = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePromptTitle = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-      end,
-      on_colors = function(colors)
-        colors.bg_statusline = "#101010"
-        colors.fg="#ffffff"
-      end
     }
 
   end,
