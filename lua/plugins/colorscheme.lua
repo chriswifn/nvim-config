99603a58e993f8fe95bf890dbc3d10f7969bb9c4
Christian Hageloch <christian.hageloch@gmail.com>
2023-05-05 13:37:44 +0200

    gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,40 @@
+-- local M = {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       style = "night",
+--       hide_inactive_statusline = true,
+--       transparent = true,
+--       styles = {
+--         keywords = { bold = true },
+--         functions = { bold = true },
+--       },
+--     }
+--   end,
+--   config = function(_, opts)
+--     vim.o.background = "dark"
+--     local tokyonight = require("tokyonight")
+--     tokyonight.setup(opts)
+--     tokyonight.load()
+--   end,
+-- }
+--
+-- return M
+
 local M = {
-  "folke/tokyonight.nvim",
+  "sainnhe/gruvbox-material",
   lazy = false,
   priority = 1000,
-  opts = function()
-    return {
-      style = "night",
-      hide_inactive_statusline = true,
-      transparent = true,
-      styles = {
-        keywords = { bold = true },
-        functions = { bold = true },
-      },
-      on_colors = function(colors)
-        colors.bg_statusline = "#101010" 
-      end,
-    }
-  end,
-  config = function(_, opts)
+  config = function()
     vim.o.background = "dark"
-    local tokyonight = require("tokyonight")
-    tokyonight.setup(opts)
-    tokyonight.load()
-  end,
+    vim.g.gruvbox_material_foreground = 'original'
+    vim.g.gruvbox_material_background = 'hard'
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.cmd("colorscheme gruvbox-material")
+  end
 }
 
 return M
