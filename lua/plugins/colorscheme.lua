5f58e67f656332fed6017c58ec44e08c63ef0981
Christian Hageloch <christian.hageloch@gmail.com>
2023-06-27 18:08:17 +0200

    lua gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,30 @@
+-- local M = {
+--   "sainnhe/gruvbox-material",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.o.background = "dark"
+--     vim.g.gruvbox_material_foreground = 'original'
+--     vim.g.gruvbox_material_transparent_background = 1
+--     vim.g.gruvbox_material_better_performance = 1
+--     vim.g.gruvbox_material_enable_bold = 1
+--     vim.cmd("colorscheme gruvbox-material")
+--   end
+-- }
+
+-- return M
+
 local M = {
-  "sainnhe/gruvbox-material",
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.o.background = "dark"
-    vim.g.gruvbox_material_foreground = 'original'
-    vim.g.gruvbox_material_transparent_background = 1
-    vim.g.gruvbox_material_better_performance = 1
-    vim.g.gruvbox_material_enable_bold = 1
-    vim.cmd("colorscheme gruvbox-material")
-  end
+  opts = {
+    transparent_mode = true,
+  },
+  config = function(_, opts)
+    require("gruvbox").setup(opts)
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
 
 return M
